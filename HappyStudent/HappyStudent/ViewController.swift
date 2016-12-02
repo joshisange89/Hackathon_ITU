1b34fc2217dc8647f9ed557df939753d14bd0bbe
Sangeeta Joshi <sangeetajoshi.ece@gmail.com>
2016-12-02 09:24:27 -0800

    Created base project

---
+++ HappyStudent/HappyStudent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappyStudent
+//
+//  Created by Santa on 12/2/16.
+//  Copyright © 2016 santa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
